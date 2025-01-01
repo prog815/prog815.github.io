cb4ac205cd3cd0639d2f84c1334f121a0b704879
Енин Алексей <eavprog@gmail.com>
2025-01-01 22:34:42 +0500

    Добавление файла

--- abscur2/contents/last_commodity_absolute_dollar_prices.js
+++ abscur2/contents/last_commodity_absolute_dollar_prices.js
@@ -1,17 +1,18 @@
 
-const lastCommodityPrices = [{"Ticker":"GC=F","Commodity Name":"\u0417\u043e\u043b\u043e\u0442\u043e","Absolute Price":60578.0376413974,"Dollar Price":2606.1000976562},{"Ticker":"SI=F","Commodity Name":"\u0421\u0435\u0440\u0435\u0431\u0440\u043e","Absolute Price":676.5605126652,"Dollar Price":29.1060009003},{"Ticker":"PL=F","Commodity Name":"\u041f\u043b\u0430\u0442\u0438\u043d\u0430","Absolute Price":20976.0257081896,"Dollar Price":902.4000244141},{"Ticker":"CL=F","Commodity Name":"\u041d\u0435\u0444\u0442\u044c WTI","Absolute Price":1650.1418217288,"Dollar Price":70.9899978638},{"Ticker":"HG=F","Commodity Name":"\u041c\u0435\u0434\u044c","Absolute Price":94.0364644789,"Dollar Price":4.0454998016},{"Ticker":"ALI=F","Commodity Name":"\u0410\u043b\u044e\u043c\u0438\u043d\u0438\u0439","Absolute Price":59163.5948463497,"Dollar Price":2545.25},{"Ticker":"ZN=F","Commodity Name":"\u0426\u0438\u043d\u043a","Absolute Price":2532.946852338,"Dollar Price":108.96875},{"Ticker":"ZW=F","Commodity Name":"\u041f\u0448\u0435\u043d\u0438\u0446\u0430","Absolute Price":12743.9115507362,"Dollar Price":548.25},{"Ticker":"ZC=F","Commodity Name":"\u041a\u0443\u043a\u0443\u0440\u0443\u0437\u0430","Absolute Price":10512.4195144924,"Dollar Price":452.25},{"Ticker":"ZS=F","Commodity Name":"\u0421\u043e\u044f","Absolute Price":22826.3039540774,"Dollar Price":982.0},{"Ticker":"KC=F","Commodity Name":"\u041a\u043e\u0444\u0435","Absolute Price":7461.5514961903,"Dollar Price":321.0},{"Ticker":"SB=F","Commodity Name":"\u0421\u0430\u0445\u0430\u0440","Absolute Price":444.6712581313,"Dollar Price":19.1299991608},{"Ticker":"NG=F","Commodity Name":"\u041f\u0440\u0438\u0440\u043e\u0434\u043d\u044b\u0439 \u0433\u0430\u0437","Absolute Price":91.4911760131,"Dollar Price":3.9360001087}];
+const lastCommodityPrices = [{"Ticker":"GC=F","Commodity Name":"\u0417\u043e\u043b\u043e\u0442\u043e","Absolute Price":61553.7747581471,"Dollar Price":2639.3000488281},{"Ticker":"SI=F","Commodity Name":"\u0421\u0435\u0440\u0435\u0431\u0440\u043e","Absolute Price":683.1016124221,"Dollar Price":29.2900009155},{"Ticker":"PL=F","Commodity Name":"\u041f\u043b\u0430\u0442\u0438\u043d\u0430","Absolute Price":20838.2134387578,"Dollar Price":893.5},{"Ticker":"CL=F","Commodity Name":"\u041d\u0435\u0444\u0442\u044c WTI","Absolute Price":1676.1527219669,"Dollar Price":71.8700027466},{"Ticker":"HG=F","Commodity Name":"\u041c\u0435\u0434\u044c","Absolute Price":93.8360951935,"Dollar Price":4.0234999657},{"Ticker":"ALI=F","Commodity Name":"\u0410\u043b\u044e\u043c\u0438\u043d\u0438\u0439","Absolute Price":58812.271672286,"Dollar Price":2521.75},{"Ticker":"ZN=F","Commodity Name":"\u0426\u0438\u043d\u043a","Absolute Price":2536.6326900391,"Dollar Price":108.765625},{"Ticker":"ZW=F","Commodity Name":"\u041f\u0448\u0435\u043d\u0438\u0446\u0430","Absolute Price":12850.4259706274,"Dollar Price":551.0},{"Ticker":"ZC=F","Commodity Name":"\u041a\u0443\u043a\u0443\u0440\u0443\u0437\u0430","Absolute Price":10698.9708058536,"Dollar Price":458.75},{"Ticker":"ZS=F","Commodity Name":"\u0421\u043e\u044f","Absolute Price":23555.2272782825,"Dollar Price":1010.0},{"Ticker":"KC=F","Commodity Name":"\u041a\u043e\u0444\u0435","Absolute Price":7482.866154462,"Dollar Price":320.8500061035},{"Ticker":"SB=F","Commodity Name":"\u0421\u0430\u0445\u0430\u0440","Absolute Price":449.8815403599,"Dollar Price":19.2900009155},{"Ticker":"NG=F","Commodity Name":"\u041f\u0440\u0438\u0440\u043e\u0434\u043d\u044b\u0439 \u0433\u0430\u0437","Absolute Price":84.7288497781,"Dollar Price":3.632999897}];
 
 function createTable() {
     const table = document.createElement('table');
+    table.style.borderCollapse = 'collapse';  // Убираем промежутки между ячейками
     table.border = '1';
 
     // Заголовок таблицы
     const header = table.createTHead();
     const headerRow = header.insertRow(0);
-    headerRow.insertCell(0).innerText = 'Ticker';
-    headerRow.insertCell(1).innerText = 'Commodity Name';
-    headerRow.insertCell(2).innerText = 'Absolute Price';
-    headerRow.insertCell(3).innerText = 'Dollar Price';
+    headerRow.insertCell(0).innerText = 'Тикер (Дата: NG=F)';  // Включаем дату в заголовок первого столбца
+    headerRow.insertCell(1).innerText = 'Название товара';
+    headerRow.insertCell(2).innerText = 'Абсолютная цена';
+    headerRow.insertCell(3).innerText = 'Долларовая цена';
 
     // Заполнение таблицы данными
     const tbody = table.createTBody();
@@ -19,8 +20,12 @@ function createTable() {
         const row = tbody.insertRow();
         row.insertCell(0).innerText = item.Ticker;
         row.insertCell(1).innerText = item['Commodity Name'];
-        row.insertCell(2).innerText = item['Absolute Price'];
-        row.insertCell(3).innerText = item['Dollar Price'];
+        row.insertCell(2).innerText = item['Absolute Price'].toFixed(2);  // Форматируем цену до двух знаков
+        row.insertCell(3).innerText = item['Dollar Price'].toFixed(2);  // Форматируем цену до двух знаков
+
+        // Выравнивание цен по правой стороне
+        row.cells[2].style.textAlign = 'right';
+        row.cells[3].style.textAlign = 'right';
     });
 
     document.getElementById('table-container').appendChild(table);
