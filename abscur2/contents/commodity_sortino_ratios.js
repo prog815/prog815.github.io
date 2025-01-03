a0c38c57ecf591794cf123cc1eb6f72a230f986a
Енин Алексей <eavprog@gmail.com>
2025-01-03 17:14:41 +0500

    Добавление файла

---
+++ abscur2/contents/commodity_sortino_ratios.js
@@ -0,0 +1,46 @@
+
+// JavaScript код для отображения диаграмм коэффициента Сортино товаров
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.getElementById("charts-container");
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/1m_commodity_sortino_ratios.png";
+    img.alt = "1m_commodity_sortino_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/1q_commodity_sortino_ratios.png";
+    img.alt = "1q_commodity_sortino_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/y2_commodity_sortino_ratios.png";
+    img.alt = "y2_commodity_sortino_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/1y_commodity_sortino_ratios.png";
+    img.alt = "1y_commodity_sortino_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/2y_commodity_sortino_ratios.png";
+    img.alt = "2y_commodity_sortino_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/3y_commodity_sortino_ratios.png";
+    img.alt = "3y_commodity_sortino_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/5y_commodity_sortino_ratios.png";
+    img.alt = "5y_commodity_sortino_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/10y_commodity_sortino_ratios.png";
+    img.alt = "10y_commodity_sortino_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+
+});
