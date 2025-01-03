895602a138acdda66c3bd6cd165b9b46333b0329
Енин Алексей <eavprog@gmail.com>
2025-01-03 16:41:06 +0500

    Добавление файла

---
+++ abscur2/contents/commodity_sharpe_ratios.js
@@ -0,0 +1,46 @@
+
+// JavaScript код для отображения диаграмм коэффициента Шарпа товаров
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.getElementById("charts-container");
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/1m_commodity_sharpe_ratios.png";
+    img.alt = "1m_commodity_sharpe_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/1q_commodity_sharpe_ratios.png";
+    img.alt = "1q_commodity_sharpe_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/y2_commodity_sharpe_ratios.png";
+    img.alt = "y2_commodity_sharpe_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/1y_commodity_sharpe_ratios.png";
+    img.alt = "1y_commodity_sharpe_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/2y_commodity_sharpe_ratios.png";
+    img.alt = "2y_commodity_sharpe_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/3y_commodity_sharpe_ratios.png";
+    img.alt = "3y_commodity_sharpe_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/5y_commodity_sharpe_ratios.png";
+    img.alt = "5y_commodity_sharpe_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+    var img = document.createElement("img");
+    img.src = "https://prog815.github.io/abscur2/contents/10y_commodity_sharpe_ratios.png";
+    img.alt = "10y_commodity_sharpe_ratios.png";
+    img.style.width = "100%";
+    container.appendChild(img);
+
+});
