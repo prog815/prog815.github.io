43404aa638ca454bbdae91978a3bf7037d41d75f
Енин Алексей <eavprog@gmail.com>
2025-02-06 20:06:07 +0500

    Добавление файла

--- abscur2/contents/pairs_distr_reit.js
+++ abscur2/contents/pairs_distr_reit.js
@@ -3,18 +3,21 @@
 const currencyPairs = ["AUDHKD", "AUDJPY", "AUDSEK", "AUDUSD", "CADJPY", "CADSEK", "CHFAUD", "CHFHKD", "CHFJPY", "EURAUD", "EURCAD", "EURCHF", "EURCNY", "EURCZK", "EURGBP", "EURHKD", "EURILS", "EURJPY", "EURNOK", "EURNZD", "EURRUB", "EURSEK", "EURSGD", "EURUSD", "EURZAR", "GBPAUD", "GBPCAD", "GBPCHF", "GBPHKD", "GBPJPY", "GBPNOK", "GBPRUB", "GBPSEK", "GBPSGD", "GBPUSD", "HKDSEK", "HKDSGD", "JPYHKD", "JPYSEK", "NZDSEK", "NZDUSD", "SGDAUD", "SGDCHF", "SGDJPY", "USDAED", "USDARS", "USDBRL", "USDCAD", "USDCHF", "USDCLP", "USDCNY", "USDCOP", "USDCZK", "USDDKK", "USDEGP", "USDGBP", "USDHKD", "USDHUF", "USDIDR", "USDILS", "USDINR", "USDISK", "USDJPY", "USDKRW", "USDKWD", "USDKZT", "USDMXN", "USDMYR", "USDNOK", "USDPEN", "USDPHP", "USDPKR", "USDPLN", "USDQAR", "USDRON", "USDRUB", "USDSAR", "USDSEK", "USDSGD", "USDTHB", "USDTRY", "USDTWD", "USDUAH", "USDVND", "USDZAR"];
 
 // Данные по временным диапазонам и соответствующим тикерам с наименьшей нормой
-const periodData = {"\u043c\u0435\u0441\u044f\u0446": {"suffix": "1m", "lowestNormTickers": ["EURSGD", "USDISK", "AUDHKD", "USDTWD", "NZDSEK"]}, "\u043a\u0432\u0430\u0440\u0442\u0430\u043b": {"suffix": "1q", "lowestNormTickers": ["GBPUSD", "EURCNY", "EURCHF", "USDPLN", "USDGBP"]}, "\u0433\u043e\u0434": {"suffix": "1y", "lowestNormTickers": ["USDCLP", "AUDSEK", "SGDCHF", "SGDJPY", "USDPLN"]}, "\u0442\u0440\u0438 \u0433\u043e\u0434\u0430": {"suffix": "3y", "lowestNormTickers": ["AUDSEK", "NZDSEK", "GBPRUB", "USDRUB", "USDKRW"]}, "\u043f\u044f\u0442\u044c \u043b\u0435\u0442": {"suffix": "5y", "lowestNormTickers": ["USDDKK", "CHFHKD", "GBPUSD", "GBPHKD", "USDPEN"]}, "\u0434\u0435\u0441\u044f\u0442\u044c \u043b\u0435\u0442": {"suffix": "10y", "lowestNormTickers": ["USDKWD", "EURUSD", "USDCAD", "USDDKK", "USDMYR"]}};
+const periodData = {"\u043c\u0435\u0441\u044f\u0446": {"suffix": "1m", "lowestNormTickers": ["USDISK", "CHFJPY", "SGDCHF", "USDTWD", "NZDSEK"], "startDate": "2025-01-06"}, "\u043a\u0432\u0430\u0440\u0442\u0430\u043b": {"suffix": "1q", "lowestNormTickers": ["USDGBP", "GBPHKD", "HKDSEK", "GBPUSD", "USDSEK"], "startDate": "2024-11-06"}, "\u0433\u043e\u0434": {"suffix": "1y", "lowestNormTickers": ["USDCLP", "AUDSEK", "SGDCHF", "SGDJPY", "USDPLN"], "startDate": "2024-02-06"}, "\u0442\u0440\u0438 \u0433\u043e\u0434\u0430": {"suffix": "3y", "lowestNormTickers": ["AUDSEK", "GBPRUB", "NZDSEK", "USDRUB", "USDKRW"], "startDate": "2022-02-06"}, "\u043f\u044f\u0442\u044c \u043b\u0435\u0442": {"suffix": "5y", "lowestNormTickers": ["SGDCHF", "CHFHKD", "GBPUSD", "GBPHKD", "USDPEN"], "startDate": "2020-02-06"}, "\u0434\u0435\u0441\u044f\u0442\u044c \u043b\u0435\u0442": {"suffix": "10y", "lowestNormTickers": ["USDKWD", "EURUSD", "USDCAD", "USDDKK", "USDMYR"], "startDate": "2015-02-06"}};
 
 // Функция для отображения диаграмм и списков тикеров
 function displayCharts() {
     const graphsContainer = document.getElementById('graphs-container');
     
     for (const [periodName, data] of Object.entries(periodData)) {
-        const {suffix, lowestNormTickers} = data;
+        const {suffix, lowestNormTickers, startDate} = data;
+
+        // Формирование заголовка с датами начала и окончания периода
+        const headerText = `За {periodName} (${startDate} - 2025-02-06)`;
 
         // Создание заголовка для временного диапазона
         const header = document.createElement('h2');
-        header.textContent = periodName;
+        header.textContent = headerText;
         graphsContainer.appendChild(header);
 
         // Создание элемента для графика
