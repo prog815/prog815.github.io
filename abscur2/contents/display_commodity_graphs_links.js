6a8bb966a28b101ab9ff24a9ea4f3553c2206344
Енин Алексей <eavprog@gmail.com>
2025-02-08 22:10:20 +0500

    Добавление файла

--- abscur2/contents/display_commodity_graphs_links.js
+++ abscur2/contents/display_commodity_graphs_links.js
@@ -39,7 +39,7 @@ function displayCommodityGraphs() {
     const graphsContainer = document.getElementById('graphs-container');
     
     // Список временных диапазонов и соответствующих суффиксов
-    const periodSuffix = {"\u043c\u0435\u0441\u044f\u0446": "1m", "\u043a\u0432\u0430\u0440\u0442\u0430\u043b": "1q", "\u043f\u043e\u043b\u0433\u043e\u0434\u0430": "y2", "\u0433\u043e\u0434": "1y", "\u0434\u0432\u0430 \u0433\u043e\u0434\u0430": "2y", "\u0442\u0440\u0438 \u0433\u043e\u0434\u0430": "3y", "\u043f\u044f\u0442\u044c \u043b\u0435\u0442": "5y", "\u0434\u0435\u0441\u044f\u0442\u044c \u043b\u0435\u0442": "10y"};
+    const periodSuffix = {"\u043c\u0435\u0441\u044f\u0446": "1m", "\u043a\u0432\u0430\u0440\u0442\u0430\u043b": "1q", "\u0433\u043e\u0434": "1y", "\u0442\u0440\u0438 \u0433\u043e\u0434\u0430": "3y", "\u043f\u044f\u0442\u044c \u043b\u0435\u0442": "5y", "\u0434\u0435\u0441\u044f\u0442\u044c \u043b\u0435\u0442": "10y"};
     
     for (const [period, suffix] of Object.entries(periodSuffix)) {
         const imgElement = document.createElement('img');
