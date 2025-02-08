3a8a8bf45bb3e24de218b48f5350f0e1994ce347
Енин Алексей <eavprog@gmail.com>
2025-02-08 21:02:11 +0500

    Добавление файла

--- abscur2/contents/display_absolute_currency_graphs_links.js
+++ abscur2/contents/display_absolute_currency_graphs_links.js
@@ -34,22 +34,16 @@ function displayAbsoluteCurrencyLinks() {
     linksContainer.innerHTML = linksHTML;
 }
 
-// Функция для отображения графиков абсолютного курса валюты
-function displayAbsoluteCurrencyGraphs() {
+// Функция для отображения графика абсолютного курса валюты
+function displayAbsoluteCurrencyGraph() {
     const graphsContainer = document.getElementById('graphs-container');
-    
-    // Список временных диапазонов и соответствующих суффиксов
-    const periodSuffix = {"\u043c\u0435\u0441\u044f\u0446": "1m", "\u043a\u0432\u0430\u0440\u0442\u0430\u043b": "1q", "\u043f\u043e\u043b\u0433\u043e\u0434\u0430": "y2", "\u0433\u043e\u0434": "1y", "\u0434\u0432\u0430 \u0433\u043e\u0434\u0430": "2y", "\u0442\u0440\u0438 \u0433\u043e\u0434\u0430": "3y", "\u043f\u044f\u0442\u044c \u043b\u0435\u0442": "5y", "\u0434\u0435\u0441\u044f\u0442\u044c \u043b\u0435\u0442": "10y"};
-    
-    for (const [period, suffix] of Object.entries(periodSuffix)) {
-        const imgElement = document.createElement('img');
-        imgElement.src = `https://prog815.github.io/abscur2/contents/${selectedTicker}_${suffix}_absolute_rate.png`;
-        imgElement.alt = `График абсолютного курса за ${period} для ${selectedTicker}`;
-        imgElement.style.width = '100%';  // Устанавливаем ширину изображения
-        graphsContainer.appendChild(imgElement);
-    }
+    const imgElement = document.createElement('img');
+    imgElement.src = `https://prog815.github.io/abscur2/contents/${selectedTicker}_absolute_rate_all_periods.png`;
+    imgElement.alt = `График абсолютного курса для ${selectedTicker}`;
+    imgElement.style.width = '100%';  // Устанавливаем ширину изображения
+    graphsContainer.appendChild(imgElement);
 }
 
-// Вызов функций для отображения ссылок и графиков
+// Вызов функций для отображения ссылок и графика
 displayAbsoluteCurrencyLinks();
-displayAbsoluteCurrencyGraphs();
+displayAbsoluteCurrencyGraph();
