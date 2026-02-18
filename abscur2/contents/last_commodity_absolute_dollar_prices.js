11b45d7a13c1d2d341a9ffddeef404c47542d4aa
Енин Алексей <eavprog@gmail.com>
2026-02-18 18:46:55 +0500

    Добавление файла

--- abscur2/contents/last_commodity_absolute_dollar_prices.js
+++ abscur2/contents/last_commodity_absolute_dollar_prices.js
@@ -12,7 +12,7 @@ function createTable() {
     const headerRow = header.insertRow(0);
     
     // Заголовки с жирным шрифтом и отступами
-    const headers = ['Тикер', 'Название товара', 'Абсолютная цена (Дата: 2026-02-16)', 'Долларовая цена (Дата: 2026-02-13)'];
+    const headers = ['Тикер', 'Название товара', 'Абсолютная цена (Дата: 2026-02-17)', 'Долларовая цена (Дата: 2026-02-13)'];
     headers.forEach((text, index) => {
         const cell = headerRow.insertCell(index);
         cell.innerText = text;
